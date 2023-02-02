434e3bbb9a9e28c9bf9c8fe2ec1c069be5e15d2e
LucaFreire <lucasrodifreire@gmail.com>
2023-02-02 10:04:46 -0300

    ToFix: link page

--- src/config/db.js
+++ src/config/db.js
@@ -1,7 +1,10 @@
 const sequelize = require('sequelize');
 //configurações da base de dados
-const database = new sequelize('LeoF', 'Falango', '154256852154256852', {
-    dialect: 'mssql', host:'localhost', port: 50181
+const database = new sequelize('take7', 'AulaJS', 'J@SqL0123_QWE',
+{
+    dialect: 'mssql', host:'localhost', port: 49721
 });
 database.sync();
 module.exports = database;
+
+// dialect: 'mssql', host:'localhost', port: 49721
